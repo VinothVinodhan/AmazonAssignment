0fce546372dd39292b729a6b510e959542c8d5b9
VinothKumar <40631480+VinothVinodhan@users.noreply.github.com>
2020-04-16 19:18:22 +0530

    Removed import
    Removed Map import which was unused

--- src/main/java/com/base/BaseSuite.java
+++ src/main/java/com/base/BaseSuite.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
